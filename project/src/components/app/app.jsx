6e3eb6d84d4a6960267c4d49981aa35664fc3f41
shantypower <a.n@alpham.pro>
2021-06-03 16:28:48 +0900

    добавляет компонент оффера и моковые данные с прокидкой через пропсы

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
 
-function App() {
-  return <MainPage/>;
+function App(props) {
+  const {offersCount, offers} = props;
+
+  return <MainPage offersCount = {offersCount} offers = {offers}/>;
 }
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+  offers: PropTypes.array.isRequired,
+};
 
 export default App;
