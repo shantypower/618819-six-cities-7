37201f8b2840efcffb84631101204321f362a1fd
shantypower <a.n@alpham.pro>
2021-06-08 21:56:34 +0900

    add mock data for reviews

---
+++ project/src/mocks/reviews.js
@@ -0,0 +1,52 @@
+const reviews = [
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:10:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/avatar-angelina.jpg',
+      'id': 4,
+      'is_pro': true,
+      'name': 'Angelina',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2020-08-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/avatar-max.jpg',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/1.png',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/1.png',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+];
+
+export default reviews;
