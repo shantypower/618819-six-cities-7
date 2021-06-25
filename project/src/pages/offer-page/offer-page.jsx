964005e94722cafa10462bed91ec94cfa33d9c57
shantypower <a.n@alpham.pro>
2021-06-25 16:56:39 +0900

    add reviews counts to offer page

--- project/src/pages/offer-page/offer-page.jsx
+++ project/src/pages/offer-page/offer-page.jsx
@@ -89,7 +89,7 @@ function OfferPage({offers, reviews}) {
                 </div>
               </div>
               <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
                 <ul className="reviews__list">
                   {reviews.map((review) => (
                     <ReviewListItem
