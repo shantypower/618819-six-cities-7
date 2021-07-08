4372ec18300819d0046788da52f5207de60dcea0
shantypower <a.n@alpham.pro>
2021-07-08 16:05:52 +0900

    add new actions

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -1,6 +1,9 @@
 export const ActionType = {
   SET_CITY: 'cities/setCity',
   SET_SORT_TYPE: 'setSortType',
+  LOAD_OFFERS: 'offers/loadOffers',
+  REQUIRED_AUTHORIZATION: 'user/requiredAuthorization',
+  LOGOUT: 'user/logout',
 };
 
 export const ActionCreator = {
@@ -11,5 +14,16 @@ export const ActionCreator = {
   setSortType: (sortType) => ({
     type: ActionType.SET_SORT_TYPE,
     payload: sortType,
+  }),
+  loadOffers: (offers) => ({
+    type: ActionType.LOAD_OFFERS,
+    payload: offers,
+  }),
+  requireAuthorization: (status) => ({
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: status,
+  }),
+  logout: () => ({
+    type: ActionType.LOGOUT,
   }),
 };
