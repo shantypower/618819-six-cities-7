285dbca19916b5415a35cf50a7141b7a29e4d5b0
shantypower <a.n@alpham.pro>
2021-07-16 17:00:03 +0900

    add MainEmpty component

---
+++ project/src/pages/main-empty/main-empty.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Header from '../../components/header/header';
+import LocationsList from '../../components/locations-list/locations-list';
+import {useSelector} from 'react-redux';
+import {getCity} from '../../store/ui/selectors';
+import {LOCATIONS} from '../../const';
+
+function MainEmpty() {
+
+  const city = useSelector(getCity);
+
+  return (
+    <div className="page page--gray page--main">
+      <Header/>
+      <main className="page__main page__main--index page__main--index-empty">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
+            <LocationsList locations={LOCATIONS}/>
+          </section>
+        </div>
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">We could not find any property available at the moment in {city}</p>
+              </div>
+            </section>
+            <div className="cities__right-section"></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default MainEmpty;
