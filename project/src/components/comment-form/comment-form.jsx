e556b4cb39919df67d5c7f658e47820b7ac22cfc
Mark Kovalenko <mkovalenko@WMCM521.local>
2021-07-30 11:10:22 +0200

    Fix comment form

--- project/src/components/comment-form/comment-form.jsx
+++ project/src/components/comment-form/comment-form.jsx
@@ -1,99 +1,73 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {useSelector, useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {sendComment} from '../../store/api-actions';
 import {MAX_REVIEW_LENGTH, MIN_REVIEW_LENGTH} from '../../const';
-import { getHasPostedComment } from '../../store/ui/selectors';
+import {getHasPostedComment} from '../../store/ui/selectors';
 
 function CommentForm({offerId}) {
+  const reviewInitialState = {
+    charsCount: 0,
+    textReview: '',
+    starRating: '5',
+  }
+  const [review, setReview] = useState(reviewInitialState)
+
+  const checkReviewValidation = (review) => {
+    const charsCount = review.charsCount;
+    return charsCount >= MIN_REVIEW_LENGTH && charsCount <= MAX_REVIEW_LENGTH;
+  };
 
   const hasPostedComment = useSelector(getHasPostedComment);
-
-  const [rating, setRating] = useState(0);
-  const [review, setReviewText] = useState('');
-  const [isDisabled, setIsDisabled] = useState(true);
 
   const dispatch = useDispatch();
 
   const handleFormChange = (evt) => {
-    setIsDisabled(!(review.length >= MIN_REVIEW_LENGTH
-      && review.length <= MAX_REVIEW_LENGTH
-      && rating > 0));
+    setReview({...review, starRating: evt.target.value});
+    evt.target.checked = ((review.starRating === evt.target.value.toString()) ? 'checked' : '');
   };
 
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
-    if (isDisabled) {
-      return;
-    }
-    setIsDisabled(true);
-    dispatch(sendComment({id: offerId, comment: review, rating: rating}))
-      .then(() => {
-        if (hasPostedComment.hasPosted) {
-          setReviewText('');
-          setRating(0);
-        } else {
-          setReviewText(hasPostedComment.comment);
-          setRating(hasPostedComment.rating);
-        }
-      })
-      .catch(() => {
-        setReviewText(review);
-        setRating(rating);
-      })
-      .finally(() => {
-        setIsDisabled(false);
-      });
+    dispatch(sendComment({id: offerId, comment: review.textReview, rating: review.starRating})).then(() =>{
+      if (hasPostedComment.hasPosted) {
+        setReview(reviewInitialState);
+      }
+    })
   };
 
   return (
-    <form className="reviews__form form" action="#" method="post" onSubmit={handleFormSubmit} onChange={handleFormChange}>
+    <form className="reviews__form form" action="#" method="post" onSubmit={handleFormSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={(evt) => setRating(Number(evt.target.value))} />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={(evt) => setRating(Number(evt.target.value))} />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={(evt) => setRating(Number(evt.target.value))} />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={(evt) => setRating(Number(evt.target.value))} />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={(evt) => setRating(Number(evt.target.value))} />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {['5', '4', '3', '2', '1'].map((index) => (
+          <React.Fragment key={index}>
+            <input onChange={handleFormChange} className="form__rating-input visually-hidden" name="rating" value={index} id={`${index}-stars`} type="radio" checked={review.starRating === index ? 'checked' : false}
+            />
+            <label htmlFor={`${index}-stars`} className="reviews__rating-label form__rating-label" title="perfect">
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star"></use>
+              </svg>
+            </label>
+          </React.Fragment>
+        ))}
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={(evt) => setReviewText(evt.target.value)} minLength={MIN_REVIEW_LENGTH} maxLength={MAX_REVIEW_LENGTH}
-        value={review}
+      <textarea className="reviews__textarea form__textarea" id="review" name="review"
+                placeholder="Tell how was your stay, what you like and what can be improved"
+                onChange={(evt) => {
+                  setReview({...review, charsCount: evt.target.value.length, textReview: evt.target.value});
+                }} minLength={MIN_REVIEW_LENGTH} maxLength={MAX_REVIEW_LENGTH}
+                value={review.textReview}
       >
       </textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay
+          with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={isDisabled}>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit"
+                disabled={!checkReviewValidation(review) ? 'disabled' : ''}>Submit
+        </button>
       </div>
     </form>
   );
