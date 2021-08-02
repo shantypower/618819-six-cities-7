7b7f8725e7199167ae3bc9ef7f8467e7518883c7
shantypower <a.n@alpham.pro>
2021-08-02 09:27:27 +0900

    fix validation in comment form

--- project/src/components/comment-form/comment-form.jsx
+++ project/src/components/comment-form/comment-form.jsx
@@ -64,7 +64,7 @@ function CommentForm({offerId}) {
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={!checkReviewValidation(review) ? 'disabled' : ''}>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={!checkReviewValidation(review) || !(+review.starRating) ? 'disabled' : ''}>Submit</button>
       </div>
     </form>
   );
